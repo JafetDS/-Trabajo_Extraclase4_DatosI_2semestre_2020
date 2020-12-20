659e865d26e6b2bbf4dfb0f9ec047721bd15aa28
Jafet Dixon Solano <jafet.dixon@gmail.com>
2020-12-20 08:01:45 -0600

    Cambio3

---
+++ Arboles/Arbol/Arbol/MainPage.xaml.cs
@@ -0,0 +1,145 @@
+﻿using Arbol.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Arbol
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public String text;
+        public Label label;
+        public StackLayout Stack;
+        public Grid grid;
+        public Entry entry;
+        public int L;
+        public int c;
+        public Editor edit;
+        public ArbolBinarioOrdenado arbole;
+
+        public MainPage()
+        {
+
+            edit = new Editor();
+            
+            arbole = new ArbolBinarioOrdenado(edit);
+            this.L = 1;
+            this.c = 0;
+            entry = new Entry();
+            grid = new Grid();
+            Stack = new StackLayout();
+            Title = "Code Button Click";
+
+          //  this.label.FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label));
+
+            entry.Keyboard = Keyboard.Numeric;
+
+            Button button = new Button
+            {
+                Text = "Insertar",
+            };
+            button.Clicked += async (sender, args) =>  Cambio();
+
+            Button button1 = new Button
+            {
+                Text = "preOrder",
+            };
+            button1.Clicked += async (sender, args) => Cambio1();
+
+            Button button2 = new Button
+            {
+                Text = "InOrder",
+            };
+            button2.Clicked += async (sender, args) => Cambio2();
+
+            Button button3 = new Button
+            {
+                Text = "PosOrder",
+            };
+            button3.Clicked += async (sender, args) => Cambio3();
+
+
+
+            Stack.Children.Add(entry);
+            Stack.Children.Add(button);
+            Stack.Children.Add(button1);
+            Stack.Children.Add(button2);
+            Stack.Children.Add(button3);
+            Stack.Children.Add(grid);
+            Stack.Children.Add(edit);
+            create();
+
+            
+
+            Content = Stack;
+   
+
+
+            // InitializeComponent();
+        }
+        
+            
+      private async void Cambio()
+       {
+
+            Label lab = new Label();
+            String text = entry.Text;
+            lab.Text = text;
+            lab.HorizontalOptions = LayoutOptions.Center;
+            grid.Children.Add(lab, c, 0);
+            this.arbole.Insertar(Int32.Parse(text));
+
+            c = c + 1;
+            if (c == L )
+            {
+                this.L = L * 2;
+                create();       
+            }
+
+
+
+            //   this.arbole.Insertar(Int32.Parse(text));
+        }
+
+        private void create()
+        {
+   
+            grid = new Grid();
+            grid.HorizontalOptions = LayoutOptions.Center;
+            for (int i = 0; i < this.L; i++)
+            {
+
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+            Stack.Children.Add(grid);
+
+            this.c = 0;
+        }
+
+
+        private async void Cambio1()
+        {
+            edit.Text = " ";
+            this.arbole.ImprimirPre();
+        }
+
+        private async void Cambio2()
+        {
+            edit.Text = " ";
+            this.arbole.ImprimirEntre();
+        }
+
+        private async void Cambio3()
+        {
+            edit.Text = " ";
+            this.arbole.ImprimirPost();
+        }
+    }
+}
---
+++ Arboles/Arbol/Arbol/ViewModels/ArbolBinarioOrdenado.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Arbol.ViewModels
+{
+    public class ArbolBinarioOrdenado
+    {
+        public List<int> list = new List<int>();
+
+        public String Con;
+        
+        public Editor edit;
+
+        public class Nodo
+        {
+            public int info;
+            public Nodo izq, der;
+        }
+        Nodo raiz;
+
+        public ArbolBinarioOrdenado(Editor editp)
+        {
+            raiz = null;
+            edit = editp;
+            Con = " ";
+        }
+
+        public void Insertar(int info)
+        {
+            this.list.Add(info);
+            this.raiz = Insertar(list.ToArray(),this.raiz, 0);
+
+        }
+
+        public Nodo Insertar(int[] arr, Nodo root, int i)
+        {
+            if (i < arr.Length)
+            {
+                Nodo temp = new Nodo();
+                temp.info = arr[i];
+                root = temp;
+
+
+                root.izq = Insertar(arr, root.izq, 2 * i + 1);
+
+
+                root.der = Insertar(arr, root.der, 2 * i + 2);
+            }
+            return root;
+        }
+
+        private void ImprimirPre(Nodo reco)
+        {
+            if (reco != null)
+            {
+                Con = reco.info.ToString();
+                edit.Text = edit.Text + Con + "-";
+                ImprimirPre(reco.izq);
+                ImprimirPre(reco.der);
+
+           
+            }
+        }
+
+        public void ImprimirPre()
+        {
+            ImprimirPre(raiz);
+
+   
+        }
+
+        private void ImprimirEntre(Nodo reco)
+        {
+            if (reco != null)
+            {
+                ImprimirEntre(reco.izq);
+
+                Con = reco.info.ToString();
+                edit.Text = edit.Text + Con + "-";
+                ImprimirEntre(reco.der);
+
+
+            }
+        }
+
+        public void ImprimirEntre()
+        {
+            ImprimirEntre(raiz);
+
+
+        }
+
+
+        private void ImprimirPost(Nodo reco)
+        {
+            if (reco != null)
+            {
+                ImprimirPost(reco.izq);
+                ImprimirPost(reco.der);
+
+                Con = reco.info.ToString();
+                edit.Text = edit.Text + Con + "-";
+            }
+        }
+
+
+        public void ImprimirPost()
+        {
+            ImprimirPost(raiz);
+            
+
+        }
+
+    }
+}
